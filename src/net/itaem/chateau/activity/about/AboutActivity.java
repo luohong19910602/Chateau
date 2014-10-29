9d9840e56d93e14172cec45cb6a9f4fae9b23381
luohong <846705189@qq.com>
2014-10-29 12:14:24 +0800

    添加关于界面

---
+++ src/net/itaem/chateau/activity/about/AboutActivity.java
@@ -0,0 +1,16 @@
+package net.itaem.chateau.activity.about;
+
+import net.itaem.chateau.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AboutActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.about_activity);
+	}
+
+	
+}
