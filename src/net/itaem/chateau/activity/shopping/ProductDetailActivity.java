0f7f3d76d0b6f2a1402dec632151ece6da93acf6
luohong <846705189@qq.com>
2014-10-29 17:21:53 +0800

    添加商品详情界面

---
+++ src/net/itaem/chateau/activity/shopping/ProductDetailActivity.java
@@ -0,0 +1,17 @@
+package net.itaem.chateau.activity.shopping;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * 商品详情界面
+ * */
+public class ProductDetailActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	
+}
